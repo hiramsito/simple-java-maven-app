a0f731e164dad806e2bb45d172d3ab66b16c6b1d
Hiram Flores <65650902+hiramsito@users.noreply.github.com>
2022-04-18 22:38:04 -0700

    Update javamavenDSL.groovy

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/hiramsito/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('hiramsito')
+            node / gitConfigEmail('bsgkoke456@gmail.com')
         }
     }
     steps {
